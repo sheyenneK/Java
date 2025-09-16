1bfdfa9c86ecc15c6d3343c5e5bac823011c459a
sheyenneK <sheyennek@nycstudents.net>
2025-09-16 16:22:49 +0000

    ...

---
+++ Java-1-sample/Java-3b-CW/Java-3b-CW/Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+ 
+ // Init function which runs when the program begins.
+ // comment 
+ void init(){ 
+// print your full name
+ System.out.println(" K sheyenne ");
+
+//Print your full name with your full address
+
+//Print your full name wiPth your full address using only one print statement (challenge).
+System.out.println("Joe Smith\n3443 corp kennedy St\nBayside\n11361");
+
+ System.out.println("Hi");
+  //
+  void init(){
+   
---
+++ Java-1-sample/Java-4-Discussion/Main.java
@@ -0,0 +1,26 @@
+class Main {
+  
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function runs when the program begins.  
+
+  void init(){
+    // Number of cars
+       
+    // Investment amount in currency
+      
+    // Person's first name
+      
+    // Is a game over?
+      
+    // Store the first initial of a name
+      
+    // Maximum number of customers in a theater (declare as a constant)
+      
+
+  }
+
+  
+}
