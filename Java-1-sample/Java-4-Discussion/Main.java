738f5e8fd9a22204bdea68a2939097a9f3988163
sheyenneK <sheyennek@nycstudents.net>
2025-09-16 16:27:55 +0000

    ...

--- Java-1-sample/Java-4-Discussion/Main.java
+++ Java-1-sample/Java-4-Discussion/Main.java
@@ -8,17 +8,17 @@ class Main {
 
   void init(){
     // Number of cars
-       
+       int numOfCars = 20; 
     // Investment amount in currency
-      
+        float investmtAmt = 200,50; 
     // Person's first name
-      
+        String flame = "Mary";
     // Is a game over?
-      
+      boolean isGameOver = false;
     // Store the first initial of a name
-      
+       char finital = " N " ;
     // Maximum number of customers in a theater (declare as a constant)
-      
+      final int MaxCust = 500 ; 
 
   }
 
